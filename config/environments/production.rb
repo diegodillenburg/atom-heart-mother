75f77278d5076ab6b91f82c451ae00ad4b805077
Diego Dillenburg Bueno <diegodillenburg@gmail.com>
2016-04-14 04:33:18 -0300

    configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Sets secret_key_base to point at environment variable
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  # config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
